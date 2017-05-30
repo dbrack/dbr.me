250b968f3422cb5e8bbbe38cb91915656e45e3fe
Dominik Brack <dominik.brack@gmail.com>
2017-05-30 10:39:58 +0200

    Use const instead of var

--- src/Icons.tsx
+++ src/Icons.tsx
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 
-var LinkedIn = require('react-icons/lib/fa/linkedin');
-var Twitter = require('react-icons/lib/fa/twitter');
-var GitHub = require('react-icons/lib/fa/github');
+/*
+	Use `require` because `tsc` doesn't find the typings when imported from `lib`.
+	We have to import it from `lib` because we use create-react-app
+*/
+const LinkedIn = require('react-icons/lib/fa/linkedin');
+const Twitter = require('react-icons/lib/fa/twitter');
+const GitHub = require('react-icons/lib/fa/github');
 
 export const TwitterIcon = styled(Twitter) ``;
 export const LinkedInIcon = styled(LinkedIn) ``;
