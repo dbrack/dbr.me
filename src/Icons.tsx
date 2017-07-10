bfa034b7201c66828a643274f747fbc87a8705da
Dominik Brack <dominik.brack@gmail.com>
2017-07-10 11:30:52 +0200

    Use square icon

--- src/Icons.tsx
+++ src/Icons.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 	Use `require` because `tsc` doesn't find the typings when imported from `lib`.
 	We have to import it from `lib` because we use create-react-app
 */
-const LinkedIn = require('react-icons/lib/fa/linkedin');
+const LinkedIn = require('react-icons/lib/fa/linkedin-square');
 const Twitter = require('react-icons/lib/fa/twitter');
 const GitHub = require('react-icons/lib/fa/github');
 
